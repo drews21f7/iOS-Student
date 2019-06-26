e964560d8ad574ce2438fcbe4cd422fb72ab2038
Drew Seeholzer <drews21f7@gmail.com>
2019-06-25 18:27:46 -0600

    Modified Post Controller

--- Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Model Controllers/PostController.swift
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Model Controllers/PostController.swift
@@ -14,9 +14,23 @@ class PostController {
     
     var posts: [Post] = []
     
-    func fetchPosts(completion: @escaping() -> Void) {
+    func fetchPosts(reset: Bool = true, completion: @escaping() -> Void) {
+        let queryEndInterval = reset ? Date().timeIntervalSince1970:
+        posts.last?.timestamp ?? Date().timeIntervalSince1970
+ 
         guard let url = baseURL else { completion(); return }
-        let getterEndpoint = url.appendingPathExtension("json")
+        let urlParameters = ["orderBy": "\"timestamp\"",
+                              "endAt":"\(queryEndInterval)", "limitToLast": "15",]
+        
+        let queryItems = urlParameters.compactMap( { URLQueryItem(name: $0.key, value: $0.value)})
+        
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        urlComponents?.queryItems = queryItems
+        
+        guard let url2 = urlComponents?.url else { completion();return}
+        
+        let getterEndpoint = url2.appendingPathExtension("json")
         var request = URLRequest(url: getterEndpoint)
         request.httpBody = nil
         request.httpMethod = "GET"
@@ -32,9 +46,13 @@ class PostController {
             do {
                 
                 let postsDictionary = try jDecoder.decode([String:Post].self, from: data)
-                var posts: [Post] = postsDictionary.compactMap({ $0.value})
-                posts.sort(by: { $0.timestamp > $1.timestamp })
-                self.posts = posts
+                let posts: [Post] = postsDictionary.compactMap({ $0.value})
+                let sortedPosts = posts.sorted(by: { $0.timestamp > $1.timestamp })
+                if reset {
+                    self.posts = sortedPosts
+                } else {
+                    self.posts.append(contentsOf: sortedPosts)
+                }
                 completion()
             } catch {
                 print (error)
